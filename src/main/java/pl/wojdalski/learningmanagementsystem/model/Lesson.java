42f6c0cf850b7d1e1d4549d17142ad413c2e24ff
Wojdalski Mateusz <wojdalski.m@gmail.com>
2020-05-07 12:04:53 +0200

    add Entity - Lesson

---
+++ src/main/java/pl/wojdalski/learningmanagementsystem/model/Lesson.java
@@ -0,0 +1,27 @@
+package pl.wojdalski.learningmanagementsystem.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String subject;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate date;
+
+    private String description;
+
+   // private List<User> users;
+}
